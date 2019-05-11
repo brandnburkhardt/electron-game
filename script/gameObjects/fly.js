0eb9f290ac237dad3498a27cca7fc19b5db2a378
Brandon Burkhardt <brandon.burkhardt@crosschx.com>
2019-05-10 22:29:33 -0400

    refactored the update for flies into the Fly file

--- script/gameObjects/fly.js
+++ script/gameObjects/fly.js
@@ -1,4 +1,7 @@
 const { GAME_HEIGHT, GAME_WIDTH } = require('../constants');
+
+const FLY_SPEED = 5;
+const FLY_SIZE = 12;
 class Fly {
   constructor() {
     this.x = Math.random() * GAME_WIDTH;
@@ -10,8 +13,25 @@ class Fly {
     this.trapped = false;
   }
 
-  update(dt, keys) {
+  update(fly, player) {
+    if (fly.trapped) {
+      const { x, y, dir, tongue } = player;
+      fly.x = x + dir * (tongue.length * Math.cos(-Math.PI / 4)) + dir * 33;
+      fly.y = y + (tongue.length * Math.sin(-Math.PI / 4)) - 23;
+    } else if (fly.target) {
+      const diffX = Math.abs(fly.x - fly.target.x);
+      const diffY = Math.abs(fly.y - fly.target.y);
+      if (diffX <= FLY_SPEED && diffY <= FLY_SPEED) {
+        fly.target = {
+          x: Math.random() * GAME_WIDTH,
+          y: Math.random() * GAME_HEIGHT,
+        };
+      }
 
+      const angle = Math.atan2(fly.target.y - fly.y, fly.target.x - fly.x);
+      fly.x += FLY_SPEED * Math.cos(angle);
+      fly.y += FLY_SPEED * Math.sin(angle);
+    }
   }
 
   draw(renderer) {
--- script/states/game.js
+++ script/states/game.js
@@ -44,24 +44,7 @@ class Game extends State {
         }
 
         this.flies.forEach(fly => {
-            if (fly.trapped) {
-                const { x, y, dir, tongue } = this.player;
-                fly.x = x + dir * (tongue.length * Math.cos(-Math.PI / 4)) + dir * 33;
-                fly.y = y + (tongue.length * Math.sin(-Math.PI / 4)) - 23;
-            } else if (fly.target) {
-                const diffX = Math.abs(fly.x - fly.target.x);
-                const diffY = Math.abs(fly.y - fly.target.y);
-                if (diffX <= FLY_SPEED && diffY <= FLY_SPEED) {
-                    fly.target = {
-                        x: Math.random() * GAME_WIDTH,
-                        y: Math.random() * GAME_HEIGHT,
-                    };
-                }
-
-                const angle = Math.atan2(fly.target.y - fly.y, fly.target.x - fly.x);
-                fly.x += FLY_SPEED*Math.cos(angle);
-                fly.y += FLY_SPEED*Math.sin(angle);
-            }
+          fly.update(fly, this.player);
         });
 
         if (this.player.ribbit.cooldown > 0) {
